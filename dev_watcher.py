c8f02b13b5fbd5793adb3bffe565659a52c368ea
Galan Montgomery <galan.c.m@gmail.com>
2016-05-15 15:02:26 -0400

    Add component building to the dev-watcher script

--- dev_watcher.py
+++ dev_watcher.py
@@ -5,6 +5,7 @@ import time
 
 
 if __name__ == "__main__":
+    Popen(["watch", "rollup -c --input components/admin.html --output=static/apps/admin.js && rollup -c --input components/index.html --output=static/apps/index.js", "components", "--interval=0.5"])
     process = Popen(["python3", "main.py", "-d"])
 
     while True:
